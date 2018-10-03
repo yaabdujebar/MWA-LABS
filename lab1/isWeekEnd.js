f7a649b0ad389acacd79427a2b5a6a2e415888ff
yaabdujebar <40149486+yaabdujebar@users.noreply.github.com>
2018-10-02 22:25:34 -0500

    Update isWeekEnd.js

--- lab1/isWeekEnd.js
+++ lab1/isWeekEnd.js
@@ -1,7 +1,8 @@
 function isWeekend(){
     const todayDate = new Date();
+    //days contains index
     const day = todayDate.getDay();
-
+//
     const days=['weekend','weekday','weekday','weekday','weekday','weekday','weekend'];
     return days[day];
     
